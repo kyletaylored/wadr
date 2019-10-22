f7355f81c0d7b0572cecfb3629733a8695a6251e
Kyle Taylor <kyletaylored@gmail.com>
2019-10-22 01:51:18 -0700

    Update index.js - reformatter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Wappalyzer = require('wappalyzer')
-const normalizeUrl = require('normalize-url')
+const Wappalyzer = require("wappalyzer");
+const normalizeUrl = require("normalize-url");
 
 // Wappalyzer options
 const options = {
@@ -8,65 +8,65 @@ const options = {
   maxUrls: 1,
   maxWait: 5000,
   recursive: false,
-  userAgent: 'Wappalyzer'
-}
+  userAgent: "Wappalyzer"
+};
 
 // Default Wappalyzer Groups
 let wapResults = {
-  '1': [], // CMS (Drupal, WordPress)
-  '22': [], // Web Server (Apache, Nginx)
-  '27': [], // Programming Language (PHP, ASP.net, etc),
-  '31': [], // CDN (CloudFlare, etc)
-  '62': [], // PaaS (Pantheon, Acquia, etc)
-  '64': [] // Reverse proxy (Nginx)
-}
+  "1": [], // CMS (Drupal, WordPress)
+  "22": [], // Web Server (Apache, Nginx)
+  "27": [], // Programming Language (PHP, ASP.net, etc),
+  "31": [], // CDN (CloudFlare, etc)
+  "62": [], // PaaS (Pantheon, Acquia, etc)
+  "64": [] // Reverse proxy (Nginx)
+};
 
 exports.helloWorld = (req, res) => {
-  res.send('Hello, World')
-}
+  res.send("Hello, World");
+};
 
 exports.processDomain = async (req, res) => {
   //   Check for URL query param
-  if (req.query.hasOwnProperty('url')) {
-    let results = []
+  if (req.query.hasOwnProperty("url")) {
+    let results = [];
     //   Normalize URL
-    let url = normalizeUrl(req.query.url)
+    let url = normalizeUrl(req.query.url);
     // Prepare Wappalyzer
-    const wappalyzer = new Wappalyzer(url, options)
+    const wappalyzer = new Wappalyzer(url, options);
     let data = await wappalyzer
       .analyze()
       .then(json => {
-        return json
+        return json;
       })
       .catch(error => {
-        console.error(error)
-        return {}
-      })
-    if (data.hasOwnProperty('applications')) {
-      processApps(data.applications)
+        console.error(error);
+        return {};
+      });
+    if (data.hasOwnProperty("applications")) {
+      processApps(data.applications);
     }
-    res.send(wapResults)
+    res.send(wapResults);
   }
-  res.send('Hello, World')
-}
+  res.send("Hello, World");
+};
 
 /**
  * Process all detected Wappalyzer applications
  * @param {array} apps An array of applications detected on domain.
  */
-function processApps (apps) {
+function processApps(apps) {
   apps.forEach(el => {
-    if (el.hasOwnProperty('categories')) {
-      let cats = el['categories']
-      cats.forEach(e => matchCategories(e, el))
+    if (el.hasOwnProperty("categories")) {
+      let cats = el["categories"];
+      cats.forEach(e => matchCategories(e, el));
     }
-  })
+  });
   // Convert arrays to strings
   Object.keys(wapResults).forEach(v => {
-    wapResults[v] = wapResults[v].join(', ')
-  })
+    wapResults[v] = wapResults[v].join(", ");
+  });
 
-  console.log(wapResults)
+  console.log(wapResults);
 }
 
 /**
@@ -74,12 +74,12 @@ function processApps (apps) {
  * @param {object} cat Category to check.
  * @param {object} app Application object with name.
  */
-function matchCategories (cat, app) {
-  let keys = Object.keys(wapResults)
+function matchCategories(cat, app) {
+  let keys = Object.keys(wapResults);
   for (let key = 0; key < keys.length; key++) {
-    const k = keys[key]
+    const k = keys[key];
     if (cat.hasOwnProperty(k)) {
-      wapResults[k].push(app['name'])
+      wapResults[k].push(app["name"]);
     }
   }
 }
